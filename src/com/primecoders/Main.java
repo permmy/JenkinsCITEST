726aab28dff8f73e2dabf8bde8637d09e6490def
permmy <gathangaperminus@gmail.com>
2019-11-27 12:23:34 +0300

    Initial push

---
+++ src/com/primecoders/Main.java
@@ -0,0 +1,11 @@
+package com.primecoders;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println("Hello perminus "+i);
+        }
+    }
+}
